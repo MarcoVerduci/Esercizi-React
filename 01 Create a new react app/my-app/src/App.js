240f4e126ffa67abb5d35af164fb912875ad765f
Marco <verduci.marco2099@gmail.com>
2023-05-29 14:16:58 +0200

    c

---
+++ 01 Create a new react app/my-app/src/App.js
@@ -0,0 +1,13 @@
+import Hello from './Components/Hello';
+import Message from './Components/Message';
+
+function App() {
+  return (
+    <>
+      <Hello />
+      <Message />
+    </>
+  );
+}
+
+export default App;
---
+++ 01 Create a new react app/my-app/src/Components/Message.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Message() {
+    return (
+        <div>
+            <h2>It's time to learn React!!!</h2>
+        </div>
+    );
+}
+
+export default Message;

363b34ff9458be43d8efafbea384888a3b23aa40
Marco <verduci.marco2099@gmail.com>
2023-05-29 15:21:05 +0200

    4

---
+++ 04 Props - Second prop/my-app/src/Components/Welcome.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Welcome extends React.Component {
+    render() {
+        const { name, age } = this.props;
+        return (
+            <div>
+                <p>Welcome, {name}!</p>
+                <p>Your age is {age}</p>
+            </div>
+        )
+    }
+}
+
+Welcome.defaultProps = {
+    name: 'Guest',
+    age: '"'
+}

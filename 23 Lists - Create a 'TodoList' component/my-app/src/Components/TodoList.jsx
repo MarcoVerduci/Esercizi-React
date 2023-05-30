ca347ac0462d3dc6be0e7d680cc4834fee6b589f
Marco <verduci.marco2099@gmail.com>
2023-05-31 00:35:15 +0200

    23

--- 23 Lists - Create a 'TodoList' component/my-app/src/Components/TodoList.jsx
+++ 23 Lists - Create a 'TodoList' component/my-app/src/Components/TodoList.jsx
@@ -11,7 +11,6 @@ export function TodoList() {
     const handleAddItem = () => {
         if (newItem.trim() !== '') {
             setItems([...items, newItem]);
-            setNewItem('');
         }
     };
 

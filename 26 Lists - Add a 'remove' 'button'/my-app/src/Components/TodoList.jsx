f893d9a4ab88a8f499b10d39708a20ab633afa2e
Marco <verduci.marco2099@gmail.com>
2023-05-31 00:53:20 +0200

    26

---
+++ 26 Lists - Add a 'remove' 'button'/my-app/src/Components/TodoList.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+export function TodoList() {
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewItem(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (newItem.trim() !== '') {
+      setItems([...items, newItem]);
+      setNewItem('');
+    }
+  };
+
+  const handleRemoveItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
+
+  const handleReset = () => {
+    setItems([]);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={newItem}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddItem}>Add</button>
+      <button onClick={handleReset}>Reset</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => handleRemoveItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};

6fbbc70cb0ac7068aa15f94e4e4f70c57bd7c28e
Marco <verduci.marco2099@gmail.com>
2023-05-31 00:33:28 +0200

    23

---
+++ 23 Lists - Create a 'TodoList' component/my-app/src/Components/TodoList.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+export function TodoList() {
+    const [items, setItems] = useState([]);
+    const [newItem, setNewItem] = useState('');
+
+    const handleInputChange = (e) => {
+        setNewItem(e.target.value);
+    };
+
+    const handleAddItem = () => {
+        if (newItem.trim() !== '') {
+            setItems([...items, newItem]);
+            setNewItem('');
+        }
+    };
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={newItem}
+                onChange={handleInputChange}
+            />
+            <button onClick={handleAddItem}>Add</button>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};

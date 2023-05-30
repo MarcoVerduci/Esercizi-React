8295550e7ae9b40b1c47b13035cc4466b2104e84
Marco <verduci.marco2099@gmail.com>
2023-05-30 23:50:11 +0200

    19

---
+++ 19 Forms - Add 'login' 'button'/my-app/src/Components/Login.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+export function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        if (username !== '' && password !== '') {
+            console.log({ username, password });
+        }
+    };
+
+    const isDisabled = username === '' || password === '';
+
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button disabled={isDisabled} onClick={handleLogin}>
+                Login
+            </button>
+        </div>
+    );
+};

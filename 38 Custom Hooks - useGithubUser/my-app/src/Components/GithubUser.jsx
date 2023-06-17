2696964d11b6d7d31df2eabbe934345bf3c112fe
Marco <verduci.marco2099@gmail.com>
2023-06-17 03:20:32 +0200

    38

--- 38 Custom Hooks - useGithubUser/my-app/src/Components/GithubUser.jsx
+++ 38 Custom Hooks - useGithubUser/my-app/src/Components/GithubUser.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
-import { useGithubUser } from './useGithubUser';
+import React, { useState, useEffect } from 'react';
+
+export function useGithubUser(username) {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await fetch(`https://api.github.com/users/${username}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setUserData(data);
+                } else {
+                    throw new Error(response.statusText);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchUserData();
+    }, [username]);
+
+    return userData;
+}
 
 export function GithubUser({ username }) {
     const userData = useGithubUser(username);

1a960981628c5e8b6eaf9115933a5353775ca71e
Marco <verduci.marco2099@gmail.com>
2023-06-20 11:42:40 +0200

    45

--- 45 React Router - Add the 'usersusername' path/my-app/src/App.js
+++ 45 React Router - Add the 'usersusername' path/my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ShowGithubUser from './Components/ShowGithubUser';
 
 export default function App() {
   return (
-    <Router>
-      <Route path="/users/:username" component={ShowGithubUser} />
-    </Router>
+    <Routes>
+      <Route path="/users/:username" Component={ShowGithubUser} />
+    </Routes>
   );
 }

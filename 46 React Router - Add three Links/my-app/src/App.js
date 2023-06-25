a3594b8c8c35fcca64d10a1ea59648f56c96e16f
Marco <verduci.marco2099@gmail.com>
2023-06-25 14:48:07 +0200

    46

--- 46 React Router - Add three Links/my-app/src/App.js
+++ 46 React Router - Add three Links/my-app/src/App.js
@@ -1,14 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import { ClickCounter } from "./Components/ClickCounter"
+import { Routes, Route, Link } from "react-router-dom";
+import { ClickCounter } from "./Components/ClickCounter";
 import ShowGithubUser from './Components/ShowGithubUser';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/home" />
-      <Route path="/users/:username" Component={ShowGithubUser} />
-      <Route path="/counter" Component={ClickCounter} />
-    </Routes>
+    <div>
+      <Link to="/home"><h1>Home</h1></Link>
+      <Link to="/users/Marco"><h1>Username</h1></Link>
+      <Link to="/counter"><h1>Counter</h1></Link>
+      
+      <Routes>
+        <Route path="/home" />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+        <Route path="/counter" element={<ClickCounter />} />
+      </Routes>
+    </div>
   );
 }
 
